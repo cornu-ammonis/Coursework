42eada976f7dba4f8a755cdd1e17cf31bc22dd63
Andrew Jones <andrewjones232@gmail.com>
2017-12-01 14:10:48 -0500

    constructor implementation, moved file

--- cs_323_java/hw5/Automata.java
+++ cs_323_java/hw5/Automata.java
@@ -6,8 +6,11 @@ PROVIDED BY THE INSTRUCTOR.  _Your Name_Here_
 
 public class Automata {
 
+	// stateByTransition[x][y] = output state if starting at state x and input is y
+	private int[][] stateByTransition;
+
 	// Constructor which builds an automata for patternString
 	public Automata(String patternString) {
-
+		stateByTransition = new int[ patternString.length() ] [ 128 ];
 	}
 }
