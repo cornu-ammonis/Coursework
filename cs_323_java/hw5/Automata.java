b32bfa31ec9e9b0367e85a84c41250613cde9c3a
Andrew Jones <andrewjones232@gmail.com>
2017-12-01 14:16:30 -0500

    populate inPattern array

--- cs_323_java/hw5/Automata.java
+++ cs_323_java/hw5/Automata.java
@@ -9,8 +9,19 @@ public class Automata {
 	// stateByTransition[x][y] = output state if starting at state x and input is y
 	private int[][] stateByTransition;
 
+	private boolean[] inPattern;
+
 	// Constructor which builds an automata for patternString
 	public Automata(String patternString) {
+		
 		stateByTransition = new int[ patternString.length() ] [ 128 ];
+
+		inPattern = new boolean[ 128 ];
+
+		// TO DO: add try catch with helpful error message if charcter not in ascii
+		for (int i = 0; i < patternString.length(); i++) 
+			inPattern[ (int) patternString.charAt(i) ] = true;
+
+		
 	}
 }
