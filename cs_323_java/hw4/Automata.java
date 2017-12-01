3798184c3b48f01342950ad51db733c28387b7ec
Andrew Jones <andrewjones232@gmail.com>
2017-12-01 13:58:58 -0500

    Automata init

---
+++ cs_323_java/hw4/Automata.java
@@ -0,0 +1,13 @@
+/*
+THIS CODE WAS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING
+CODE WRITTEN BY OTHER STUDENTS OR SOURCES OUTSIDE OF THOSE
+PROVIDED BY THE INSTRUCTOR.  _Your Name_Here_
+*/
+
+public class Automata {
+
+	// Constructor which builds an automata for patternString
+	public Automata(String patternString) {
+
+	}
+}
