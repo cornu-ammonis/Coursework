ec5c5ee8eaafc65b81cb030cf120fdf5a12c788b
Andrew Jones <andrewjones232@gmail.com>
2017-12-02 12:07:37 -0500

    testing of isFinal

--- cs_323_java/hw5/TestAutomata.java
+++ cs_323_java/hw5/TestAutomata.java
@@ -3,7 +3,7 @@ public class TestAutomata {
 	public static void main(String[] args) {
 
 		String pattern1 = "aba;alksdjfcccabefagasdf;lkj";
-		String test1 = "aba;alksdjfcccabesfagasdf;lkjaba;alksdjfccccabefagasdf;lkjaba;alksdjfcccsabefagsasdf;lkj";
+		String test1 = "aba;alksdjfcccabesfagasdf;lkjaba;alksdjfccccaba;alksdjfcccabefagasdf;lkjabefagasdf;lkjaba;alksdjfcccsabefagsasdf;lkj";
 
 		Automata testAutomata = new Automata(pattern1);
 
@@ -16,5 +16,19 @@ public class TestAutomata {
 		}
 
 		System.out.println("Current = " + current + testAutomata.isFinal(current));
+
+		if (testAutomata.isFinal(current)) {
+			boolean works = true;
+			while (i < test1.length()) {
+				if (current != testAutomata.transition(current, test1.charAt(i))) {
+					System.out.println("isFinal is broken! changed anyway.");
+					works = false;
+				}
+			
+				i++;
+			}
+
+			System.out.println("isfinal works: " + works);
+		}
 	}
 }
